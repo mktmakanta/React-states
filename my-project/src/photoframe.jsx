36b4bcf7c7b71beea0d50e989f3809fcb88b3a0e
makanta <mktmakantamkt@gmail.com>
2024-05-06 21:51:50 +0100

    moving on to react useEffect

--- my-project/src/photoframe.jsx
+++ my-project/src/photoframe.jsx
@@ -15,14 +15,14 @@ function handlePreviousClick(){
   let image = pictures[imagePresent];
   return (
     <>
-    <div className="">
-      <h2 className="">Our Products</h2>
-      <div className="p-5 h-98 w-screen rounded-2xl">
-        <img className="h-98 w-10/12 mx-auto rounded-2xl" key={image.id} src={`src/images/${image.image}.jpg`} alt={image.name} />
+    <div className=" bg-slate-300   ">
+      <div className="p-5 h-fit w-screen rounded-2xl ">
+      <h2 className="text-center mt-10 font-bold text-3xl">Our Products</h2>
+        <img className="h-98 w-10/12 mx-auto rounded-2xl mt-5" key={image.id} src={`src/images/${image.image}.jpg`} alt={image.name} />
       </div>
       </div>
       
-      <div className="grid grid-cols-4 text-2xl mt-10 justify-between mx-16 font-bold bg-slate-300 py-4 px-5 items-center text-center ">
+      <div className="grid grid-cols-4 text-2xl mt-2 justify-between  font-bold w-screen bg-slate-300 py-4 px-5 items-center text-center ">
         <button className="bg-slate-100 p-2 rounded-md col-span-1" onClick={handlePreviousClick} >Previous</button>
         <div className="uppercase font-bold col-span-2">{image.name}</div>
         <button  className="bg-slate-100 p-2 rounded-md col-span-1" onClick={handleNextClick}>Next</button>
